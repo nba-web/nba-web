b807156f2c4b60ef7dab3b5479871bd017f8eebc
JUN JIA <JUN JIA>
2017-08-03 18:27:29 +0800

    m/index btn

--- m/js/index.js
+++ m/js/index.js
@@ -1,13 +1,17 @@
 function btn1($option="open"){
 	$login_slt = document.getElementById('login_slt').style; 
+	$invite = document.getElementById('succeed').style; 
 	if($option=="open"){
 		//alert("1234");
 		if($login_slt.display == 'block')
 			$login_slt.display='none';
 		else
 			$login_slt.display='block';
-	}else if ($option=="close"){
-		
-		
+	}else if ($option=="close_invite"){
+		$invite.display='none';
+	}
+	else if($option=="open_invite"){
+		alert("open_invite");
+		$invite.display='block';
 	}
 }
